b3df4651ce860b277f4c05dedba1da2a2e081417
Tia Neally <57420836+lumincity@users.noreply.github.com>
2022-08-19 11:47:13 -0500

    Calculator.py
    Added comments

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-
+#defining the different types of operations
 def add(x, y):
    return x + y
 
@@ -11,7 +11,7 @@ def multi(x, y):
 def divi(x, y):
     return x / y
 
-
+#options for calculator
 print("Please Select and Operation")
 
 print(" Enter 'a' for Addition")
@@ -25,26 +25,26 @@ print(" Enter 'd' for Division")
 
 while True:
     
-    c = input("Enter a choice: ")
-
-    if c in ("a", "s", "m", "d"):
+    c = input("Enter a choice: ")   #gets choice from user
+     
+    if c in ("a", "s", "m", "d"):            #all of the choices require getting the numbers first
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
         if c == 'a':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, "=", add(num1, num2))    #addition
 
         elif c == 's':
-             print(num1, "-", num2, "=", sub(num1, num2))
+             print(num1, "-", num2, "=", sub(num1, num2))   #subtraction
 
         elif c == "m":
-            print(num1, "*", num2, "=", multi(num1, num2))
+            print(num1, "*", num2, "=", multi(num1, num2))  #multiplication
 
         elif c == "d":
-            print(num1, "/", num2, "=", divi(num1, num2))
+            print(num1, "/", num2, "=", divi(num1, num2))   #division
 
-        again = input("Want to keep going? Y/N?  ")
+        again = input("Want to keep going? Y/N?  ")      #if user says n this will break off and quit the code
         if again == "n":
           break
         else:
-          print("Invalid Input. Please try again")
+          print("Invalid Input. Please try again")       #starts back from the beginning of code after this
